acf2ac6534d5b8c42b4e9f7cfa125e3410a87adb
neelthepatel8 <patel.neel5@northeastern.edu>
2025-03-15 12:42:36 -0400

    add curl generator to dependancies

--- app/api/dependancies.py
+++ app/api/dependancies.py
@@ -1,9 +1,10 @@
 from fastapi import UploadFile, File, HTTPException
-from app.services import HarParserService, LLMService
+from app.services import HarParserService, LLMService, CurlGeneratorService
 
 # defining outside in global scope so we only have 1 instance of the singleton
 _har_parser = None
 _llm_service = None
+_curl_generator = None
 
 
 def get_har_parser():
@@ -18,6 +19,13 @@ def get_llm_service():
     if _llm_service is None:
         _llm_service = LLMService()
     return _llm_service
+
+
+def get_curl_generator():
+    global _curl_generator
+    if _curl_generator is None:
+        _curl_generator = CurlGeneratorService()
+    return _curl_generator
 
 
 def validate_har(file: UploadFile = File(...)):
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,5 @@
 from .llmservice import LLMService
 from .har_parser import HarParserService
+from .curl_generator import CurlGeneratorService
 
-__all__ = ["LLMService", "HarParserService"]
+__all__ = ["LLMService", "HarParserService", "CurlGeneratorService"]
