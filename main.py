96cdf316ef118ddc4c9b9ae6fa806e855d1c01d4
neelthepatel8 <patel.neel5@northeastern.edu>
2025-03-15 16:58:40 -0400

    open cors to all origins

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import app as api_router
 
 app = FastAPI(
@@ -6,5 +7,16 @@ app = FastAPI(
     description="API for reverse engineering API requests from HAR files",
     version="1.0.0",
 )
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.mount("/", api_router)
