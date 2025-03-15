30ff77499ed55f861729a444e0d2270f46edd348
neelthepatel8 <patel.neel5@northeastern.edu>
2025-03-15 12:31:46 -0400

    add har parser to routes to test basic parsing.

--- app/api/dependancies.py
+++ app/api/dependancies.py
@@ -1,4 +1,23 @@
 from fastapi import UploadFile, File, HTTPException
+from app.services import HarParserService, LLMService
+
+# defining outside in global scope so we only have 1 instance of the singleton
+_har_parser = None
+_llm_service = None
+
+
+def get_har_parser():
+    global _har_parser
+    if _har_parser is None:
+        _har_parser = HarParserService()
+    return _har_parser
+
+
+def get_llm_service():
+    global _llm_service
+    if _llm_service is None:
+        _llm_service = LLMService()
+    return _llm_service
 
 
 def validate_har(file: UploadFile = File(...)):
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, UploadFile, Depends, Form
 
-from .dependancies import validate_har
+from .dependancies import (
+    validate_har,
+    get_har_parser,
+    get_llm_service,
+    HarParserService,
+    LLMService,
+)
 
 app = APIRouter()
 
@@ -12,8 +18,20 @@ def health():
 
 @app.post("/api/process-har")
 async def process_har(
-    file: UploadFile = Depends(validate_har), api_description: str = Form(...)
+    file: UploadFile = Depends(validate_har),
+    api_description: str = Form(...),
+    har_parser: HarParserService = Depends(get_har_parser),
+    llm_service: LLMService = Depends(get_llm_service),
 ):
+    content = await file.read()
+    requests = har_parser.extract_requests(content)
+
+    if not requests:
+        return {"message": "No API Requests found in the har file."}
+
+    matched_request = llm_service.identify_request(requests, api_description)
+
     return {
-        "message": f"Received file: {file.filename} and description: {api_description}"
+        "message": f"Found matching API request",
+        "matched_request": matched_request,
     }
